28a162fe2ed615165f89b00a3e374b2680b3c445
Emilien Leroy <emilien1005@hotmail.fr>
2023-05-04 07:32:51 +0200

    ✨ List all glb file from the public folder
    Allow users to select a different model to wathc

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,22 @@
 import './style.css';
 import '@google/model-viewer';
+import { ModelViewerElement } from '@google/model-viewer';
+
+const models = Object.keys(import.meta.glob('../public/*.glb', { as: 'url'}));
+const viewer = document.querySelector('#viewer') as ModelViewerElement;
+const previews = document.querySelector('#models') as HTMLDivElement;
+
+function getUrl(path: string) {
+  const names = path.split('/');
+  return `/${names[names.length - 1]}`;
+}
+
+models.forEach((path) => {
+  const url = getUrl(path);
+  const preview = new ModelViewerElement();
+
+  preview.src = url;
+  preview.addEventListener('click', () => viewer.src = url);
+
+  previews.append(preview);
+});
